9def380a4f40efc370f0fb4fc5e64bc2d11ca642
Davith Chhung <chhungdavith@gmail.com>
2022-06-20 14:41:01 +0200

    add sign up and sign in form

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,39 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import SignIn from "../views/SignIn.vue";
+import SignUp from "../views/SignUp.vue";
+import About from "../views/About.vue";
+import Todo from "../views/Todo.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: Home,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+      path: "/signup",
+      name: "signup",
+      component: SignUp,
+    },
+    {
+      path: "/signin",
+      name: "signin",
+      component: SignIn,
+    },
+    {
+      path: "/about",
+      name: "about",
+      component: About,
+    },
+    {
+      path: "/todo",
+      name: "todo",
+      component: Todo,
+    },
+  ],
+});
 
-export default router
+export default router;
