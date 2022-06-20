e879eafbb6802057eea898271f1e459397f133c5
Davith Chhung <chhungdavith@gmail.com>
2022-06-20 16:55:29 +0200

    add update string

--- src/views/CRUD.vue
+++ src/views/CRUD.vue
@@ -1,7 +1,8 @@
 <script setup>
 import { ref, onMounted, watchEffect } from "vue";
 import axios from "axios";
-const listOfString = ref(null);
+const listOfString = ref({});
+const stringToUpdate = ref({});
 const newString = ref(localStorage.getItem("currentInput") || "");
 onMounted(() => {
   const fetchAllString = async () => {
@@ -27,15 +28,40 @@ const handleAddString = async () => {
     console.log(error);
   }
 };
+
+const getStringToUpdate = (index, id) => {
+  stringToUpdate.value = { string: listOfString.value[index].string, id: id };
+  console.log(stringToUpdate.value);
+};
+
+const updateString = async (id) => {
+  const response = await axios.put(
+    "http://localhost:4000/string/update/" + id,
+    {
+      string: stringToUpdate.value.string,
+    }
+  );
+  console.log(response.data);
+};
 </script>
 <template>
   <h1>CRUD</h1>
-  <ul v-for="element in listOfString">
-    <li>{{ element }}</li>
+  <ul v-for="(element, index) in listOfString">
+    <li @click="getStringToUpdate(index, element.id)">{{ element.string }}</li>
   </ul>
+  <div v-if="Object.keys(stringToUpdate).length > 0" class="my-3">
+    <input
+      type="text"
+      v-model="stringToUpdate.string"
+      class="form-control mb-2"
+    />
+    <button class="btn btn-warning" @click="updateString(stringToUpdate.id)">
+      update
+    </button>
+  </div>
   <input
     type="text"
-    placeholder="Saisisez une chaîne de caractères ..."
+    placeholder="Saisisez une chaîne de caractères..."
     class="form-control mb-3"
     v-model="newString"
   />
